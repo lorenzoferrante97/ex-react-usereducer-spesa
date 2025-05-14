1552bfd78c0fe8d5a7e39004402ddd067f5bac53
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-14 17:35:23 +0200

    add: products array

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -5,7 +5,14 @@ import { createContext, useState, useContext } from 'react';
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
-  const value = {};
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
+  const value = { products };
 
   return <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>;
 };
