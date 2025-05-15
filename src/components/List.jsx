c56a9a25de3a25a4e27b499b1b14ca35addb04ac
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-15 12:12:16 +0200

    add: empty list state

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,32 +6,36 @@ export default function List({ products, type }) {
   return (
     <>
       <ul>
-        {products.map((prod, i) => {
-          const { name, price } = prod;
+        {products.length != 0 ? (
+          products.map((prod, i) => {
+            const { name, price } = prod;
 
-          return (
-            <li key={i} className="list-items">
-              <p>
-                Nome: <span>{name}</span>
-              </p>
-              <p>
-                Prezzo: <span>{price.toFixed(2)}€</span>
-              </p>
-              {type == 'cart' && (
+            return (
+              <li key={i} className="list-items">
                 <p>
-                  Quantità: <span>{prod.quantity}</span>
+                  Nome: <span>{name}</span>
                 </p>
-              )}
-              {type == 'products' && (
-                <div>
-                  <button onClick={() => addToCart(prod)} className="btn btn-primary">
-                    Aggiungi al Carrello
-                  </button>
-                </div>
-              )}
-            </li>
-          );
-        })}
+                <p>
+                  Prezzo: <span>{price.toFixed(2)}€</span>
+                </p>
+                {type == 'cart' && (
+                  <p>
+                    Quantità: <span>{prod.quantity}</span>
+                  </p>
+                )}
+                {type == 'products' && (
+                  <div>
+                    <button onClick={() => addToCart(prod)} className="btn btn-primary">
+                      Aggiungi al Carrello
+                    </button>
+                  </div>
+                )}
+              </li>
+            );
+          })
+        ) : (
+          <p>Non sono presenti prodotti nella lista.</p>
+        )}
       </ul>
     </>
   );
